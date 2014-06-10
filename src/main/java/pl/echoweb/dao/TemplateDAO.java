74bd64155c77cb5c95d89ef8e5c15255bbcfdbc8
Rafal <rafik991@gmail.com>
2014-06-10 23:44:33 +0200

    Implementacje mem cache.

--- src/main/java/pl/echoweb/dao/TemplateDAO.java
+++ src/main/java/pl/echoweb/dao/TemplateDAO.java
@@ -1,80 +1,84 @@
 package pl.echoweb.dao;
 
-import java.util.List;
-
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 import pl.echoweb.model.entity.TemplateEntity;
 import pl.echoweb.util.MemCachedProvider;
 
+import java.util.List;
+
 /**
  * Klasa implementujaca interfejs {@link ITemplateDAO}.
- * 
+ *
  * @author rafal.machnik
- * 
  */
 @Repository("TemplateDAO")
 public class TemplateDAO implements ITemplateDAO {
 
-	@Autowired
-	private SessionFactory mySessionFactory;
+    @Autowired
+    private SessionFactory mySessionFactory;
 
     @Autowired
     MemCachedProvider memCachedProvider;
 
-	@Override
-	public TemplateEntity getTemplate(Long id) {
-		Session session = mySessionFactory.getCurrentSession();
-		TemplateEntity template = (TemplateEntity) session.get(
-				TemplateEntity.class, id);
-		return template;
-	}
+    @Override
+    public TemplateEntity getTemplate(Long id) {
+        Session session = mySessionFactory.getCurrentSession();
+        TemplateEntity template = (TemplateEntity) memCachedProvider.getObjectFromMemCache(String.valueOf(id));
+        if (template == null)
+            template = (TemplateEntity) session.get(
+                    TemplateEntity.class, id);
+        return template;
+    }
 
-	@Override
-	public Long addTemplate(TemplateEntity template) {
-		Session session = mySessionFactory.getCurrentSession();
-		return (Long) session.save(template);
-	}
+    @Override
+    public Long addTemplate(TemplateEntity template) {
+        Session session = mySessionFactory.getCurrentSession();
+        final Long id = (Long) session.save(template);
+        memCachedProvider.storeObjectInMemCache(id.toString(), template);
+        return id;
+    }
 
-	@Override
-	public List<TemplateEntity> getTemplates() {
-		Session session = mySessionFactory.getCurrentSession();
-		Criteria myCriteria = session.createCriteria(TemplateEntity.class);
-		@SuppressWarnings("unchecked")
-		List<TemplateEntity> list = myCriteria.list();
-		return list;
-	}
+    @Override
+    public List<TemplateEntity> getTemplates() {
+        Session session = mySessionFactory.getCurrentSession();
+        Criteria myCriteria = session.createCriteria(TemplateEntity.class);
+        List<TemplateEntity> list = (List<TemplateEntity>) memCachedProvider.getObjectFromMemCache("templateEntityList");
+        if (list == null || list.isEmpty())
+                    list = myCriteria.list();
+        return list;
+    }
 
-	@Override
-	public void updateTemplate(TemplateEntity template) {
-		Session session = mySessionFactory.getCurrentSession();
-		session.update(template);
-	}
+    @Override
+    public void updateTemplate(TemplateEntity template) {
+        Session session = mySessionFactory.getCurrentSession();
+        session.update(template);
+        memCachedProvider.storeObjectInMemCache(template.getId().toString(), template);
+    }
 
-	@Override
-	public void deleteTemplate(Long id) {
-		Session session = mySessionFactory.getCurrentSession();
-		TemplateEntity ent = getTemplate(id);
-		session.delete(ent);
-	}
+    @Override
+    public void deleteTemplate(Long id) {
+        Session session = mySessionFactory.getCurrentSession();
+        TemplateEntity ent = getTemplate(id);
+        session.delete(ent);
+    }
 
-	@Override
-	public String getHeader(Long id) {
-		Session session = mySessionFactory.getCurrentSession();
-		TemplateEntity template = (TemplateEntity) session.get(
-				TemplateEntity.class, id);
-		return template.getHeader();
-	}
+    @Override
+    public String getHeader(Long id) {
+        Session session = mySessionFactory.getCurrentSession();
+        TemplateEntity template = (TemplateEntity) session.get(
+                TemplateEntity.class, id);
+        return template.getHeader();
+    }
 
-	@Override
-	public String getFooter(Long id) {
-		Session session = mySessionFactory.getCurrentSession();
-		TemplateEntity template = (TemplateEntity) session.get(
-				TemplateEntity.class, id);
-		return template.getFooter();
-	}
+    @Override
+    public String getFooter(Long id) {
+        Session session = mySessionFactory.getCurrentSession();
+        TemplateEntity template = (TemplateEntity) session.get(
+                TemplateEntity.class, id);
+        return template.getFooter();
+    }
 }
