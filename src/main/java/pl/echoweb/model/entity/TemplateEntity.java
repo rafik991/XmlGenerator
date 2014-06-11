e336966600db5c6243ebd372408a3f3401ad7496
Rafal <rafik991@gmail.com>
2014-06-12 00:02:32 +0200

    Serializacja Template.

--- src/main/java/pl/echoweb/model/entity/TemplateEntity.java
+++ src/main/java/pl/echoweb/model/entity/TemplateEntity.java
@@ -1,94 +1,87 @@
 package pl.echoweb.model.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  * Encja zawierajaca szablony.
- * 
+ *
  * @author rafal.machnik
- * 
  */
 
 @Entity
 @Table(name = "TEMPLATES")
-public class TemplateEntity {
+public class TemplateEntity implements Serializable {
 
-	@Id
-	@SequenceGenerator(name = "template_seq", sequenceName = "template_id_seq", allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "template_seq")
-	@Column(name = "ID")
-	private Long id;
+    @Id
+    @SequenceGenerator(name = "template_seq", sequenceName = "template_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "template_seq")
+    @Column(name = "ID")
+    private Long id;
 
-	@Column(name = "Template_Name")
-	private String templateName;
+    @Column(name = "Template_Name")
+    private String templateName;
 
-	@Lob
-	@Column(name = "Template")
-	private byte[] template;
+    @Lob
+    @Column(name = "Template")
+    private byte[] template;
 
-	@Lob
-	@Column(name = "Excel")
-	private byte[] excel;
+    @Lob
+    @Column(name = "Excel")
+    private byte[] excel;
 
-	@Column(name = "Header")
-	private String header;
+    @Column(name = "Header")
+    private String header;
 
-	@Column(name = "Footer")
-	private String footer;
+    @Column(name = "Footer")
+    private String footer;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public byte[] getTemplate() {
-		return template;
-	}
+    public byte[] getTemplate() {
+        return template;
+    }
 
-	public void setTemplate(byte[] template) {
-		this.template = template;
-	}
+    public void setTemplate(byte[] template) {
+        this.template = template;
+    }
 
-	public String getTemplateName() {
-		return templateName;
-	}
+    public String getTemplateName() {
+        return templateName;
+    }
 
-	public void setTemplateName(String templateName) {
-		this.templateName = templateName;
-	}
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
 
-	public byte[] getExcel() {
-		return excel;
-	}
+    public byte[] getExcel() {
+        return excel;
+    }
 
-	public void setExcel(byte[] excel) {
-		this.excel = excel;
-	}
+    public void setExcel(byte[] excel) {
+        this.excel = excel;
+    }
 
-	public String getHeader() {
-		return header;
-	}
+    public String getHeader() {
+        return header;
+    }
 
-	public void setHeader(String header) {
-		this.header = header;
-	}
+    public void setHeader(String header) {
+        this.header = header;
+    }
 
-	public String getFooter() {
-		return footer;
-	}
+    public String getFooter() {
+        return footer;
+    }
 
-	public void setFooter(String footer) {
-		this.footer = footer;
-	}
+    public void setFooter(String footer) {
+        this.footer = footer;
+    }
 
 }
